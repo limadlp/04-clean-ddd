c97d7ba9386ccb1ead7f565d16ef3ed68c29a62c
limadlp <dlplima@hotmail.com>
2024-05-13 18:19:37 -0300

    feat: add delete and edit questions and answers

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -2,7 +2,8 @@ import { Question } from "../../enterprise/entities/question";
 
 export interface QuestionsRepository{
     findById(id: string) : Promise<Question | null>
-    create(question: Question): Promise<void>;
     findBySlug(slug: string) : Promise<Question | null>
+    save(question: Question): Promise<void>;
+    create(question: Question): Promise<void>;
     delete(question: Question) : Promise<void>
 }
---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,58 @@
+import { InMemoryAnswersRepository } from "./../../../../../test/repositories/in-memory-answers-repository";
+import { expect, describe, beforeEach, it } from "vitest";
+import { makeAnswer } from "test/factories/make-answer";
+import { EditAnswerUseCase } from "./edit-answer";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: EditAnswerUseCase;
+
+describe("Edit Answer", () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository);
+  });
+
+  it("should be able to edit a answer", async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID("author-1"),
+      },
+      new UniqueEntityID("answer-1")
+    );
+
+    inMemoryAnswersRepository.create(newAnswer);
+
+    await sut.execute({
+      answerId: newAnswer.id.toValue(),
+      authorId: "author-1",
+      content: "Conteúdo teste",
+
+    });
+
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+        title: "Pergunta teste",
+        content: "Conteúdo teste",
+    });
+  });
+
+  it("should not be able to edit a answer from another user", async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID("author-1"),
+      },
+      new UniqueEntityID("answer-1")
+    );
+
+    inMemoryAnswersRepository.create(newAnswer);
+
+    expect(async () => {
+      return await sut.execute({
+        answerId: newAnswer.id.toValue(),
+        authorId: "author-2",
+        content: "Conteúdo teste",
+  
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+});
