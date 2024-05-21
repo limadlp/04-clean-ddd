9ffddbcbb3594e7e6634896eef8dd8600e20a976
limadlp <dlplima@hotmail.com>
2024-05-21 19:58:59 -0300

    feat: add send notification usecase

---
+++ test/repositories/in-memory-notifications-repository.ts
@@ -0,0 +1,13 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
+import { NotificationsRepository } from "@/domain/notification/application/repositories/notifications-repository";
+import { Notification } from "@/domain/notification/enterprise/entities/notification";
+
+export class InMemoryNotificationsRepository
+  implements NotificationsRepository
+{
+  public items: Notification[] = [];
+
+  async create(notification: Notification) {
+    this.items.push(notification);
+  }
+}
